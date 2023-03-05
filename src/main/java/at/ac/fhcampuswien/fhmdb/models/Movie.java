6716cf040ad6462110b2c366efd2cc926f72de60
MBGamesStudios <azeigs-github@yahoo.com>
2023-03-05 11:32:22 +0100

    reworked Genre class to Genre enum

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -1,36 +1,38 @@
 package at.ac.fhcampuswien.fhmdb.models;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumSet;
 import java.util.List;
+
+import static at.ac.fhcampuswien.fhmdb.models.Genre.*;
 
 public class Movie {
     private String title;
     private String description;
+    private EnumSet<Genre> genres;
 
-    //private List<Genre> genres = Genre.genres;
-    private String genres;
-
-    public Movie(String title, String description, String genres) {
+    public Movie(String title, String description, Genre... genres) {
         this.title = title;
         this.description = description;
-        this.genres = genres;
+        this.genres = EnumSet.copyOf(Arrays.asList(genres));
     }
 
     public String getTitle() {
         return title;
     }
-
     public String getDescription() {
         return description;
     }
+    public EnumSet<Genre> getGenres() { return genres; }
 
-    public static List<Movie> initializeMovies(){
+    public static List<Movie> initializeMovies() {
         List<Movie> movies = new ArrayList<>();
         // TODO add some dummy data here
 
-        movies.add(new Movie("test1", "des", "genre"));
-        movies.add(new Movie("test2", "des", "genre"));
-        movies.add(new Movie("test3", "des", "genre"));
+        movies.add(new Movie("test1", "des1", ACTION));
+        movies.add(new Movie("test2", "des2", ADVENTURE, BIOGRAPHY));
+        movies.add(new Movie("test3", "des3", ANIMATION));
 
         return movies;
     }
