6e4c453981b3ba60fd30db224d13236f887f6824
MBGamesStudios <azeigs-github@yahoo.com>
2023-04-14 15:45:51 +0200

    added test files (MovieAPITest)

---
+++ src/test/java/at/ac/fhcampuswien/fhmdb/MovieAPITest.java
@@ -0,0 +1,8 @@
+package at.ac.fhcampuswien.fhmdb;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MovieAPITest {
+    private final HomeController controller = new HomeController();
+}
