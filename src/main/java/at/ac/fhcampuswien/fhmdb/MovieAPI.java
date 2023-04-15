190e243f2fd9d2c77b392c65223d266c4563f6a0
FHStudent <91724473+FHStudent@users.noreply.github.com>
2023-04-15 16:52:02 +0200

    Added request generator functionality

--- src/main/java/at/ac/fhcampuswien/fhmdb/MovieAPI.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/MovieAPI.java
@@ -2,15 +2,43 @@ package at.ac.fhcampuswien.fhmdb;
 
 import at.ac.fhcampuswien.fhmdb.model.Movie;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import okhttp3.*;
 
 public class MovieAPI {
+
     public List<Movie> get(String url) {
         return null;
     }
 
-    private String requestGenerator() {
+    private String requestGenerator() throws IOException {
         //TODO: Eduard
+
+        OkHttpClient client = new OkHttpClient();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("http://prog2.fh-campuswien.ac.at/movies").newBuilder();
+        //urlBuilder.addQueryParameter("");
+        //urlBuilder.addQueryParameter("");
+        String url = urlBuilder.toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+
+        } catch (IOException e) {
+            PrintStream errorOut = new PrintStream("error.txt");
+            System.setErr(errorOut);
+        }
+
         return null;
     }
 
